8214604905b9aee5a34accf232d42471dc249d3b
Attckturtle <89983007+Attckturtle@users.noreply.github.com>
2022-03-24 19:06:50 +0800

    Add files via upload

--- bankingApplication/bankingApplication/bankingApplication.cpp
+++ bankingApplication/bankingApplication/bankingApplication.cpp
@@ -2,13 +2,20 @@
 #include <string>
 #include <iomanip>
 #include <list>
+#include <algorithm>
+#include <vector>
+#include <iterator>
 using namespace std;
+
+string enteredUsername;
+string enteredPassword;
 
 bool makingAccount;
 bool isAccountCorrect;
 int chosenOption;
-list<string>listOfAccountUsernames = {""};
-list<string>listOfAccountPasswords = {""};
+vector<string> listOfAccountUsernames;
+vector<string> listOfAccountPasswords;
+vector<int> listOfDepositedValues;
 
 int main()
 {
@@ -17,10 +24,14 @@ int main()
     cout << "Pick an option\n";
     cin >> chosenOption;
     processChosenOption();
+
+    return 0;
 }
 
-void processChosenOption() {
-    switch (chosenOption) {
+void processChosenOption()
+{
+    switch (chosenOption)
+    {
     case 1:
         makeAccount();
         makingAccount = true;
@@ -29,16 +40,17 @@ void processChosenOption() {
         depositAnAmount();
         break;
     default:
-        cout << "Please pick a valid option";
+        cout << "Please pick a valid option\n";
     }
 }
 
-void makeAccount() {
-    cout << "Enter a username";
+void makeAccount()
+{
+    cout << "Enter a username\n";
     string userName;
     cin >> userName;
 
-    cout << "Enter a password";
+    cout << "Enter a password\n";
     string password;
     cin >> password;
 
@@ -46,20 +58,56 @@ void makeAccount() {
     listOfAccountPasswords.push_back(password);
 }
 
-void depositAnAmount() {
+void depositAnAmount()
+{
     string enteredUsername;
-    cout << "Enter your username";
+    cout << "Enter your username\n";
     cin >> enteredUsername;
 
     string enteredPassword;
-    cout << "Enter your password";
+    cout << "Enter your password\n";
     cin >> enteredPassword;
 
-    isAccountCorrect = checkIfCorrectAccount(enteredUsername, enteredPassword);
+    if (checkIfCorrectAccount(enteredUsername, enteredPassword))
+    {
+        int amountYouWantToDeposit;
+        cout << "How much would you like to deposit?\n";
+        cin >> amountYouWantToDeposit;
+        int indexOfThing = indexOfWantedAccount(enteredUsername);
+        listOfDepositedValues.push_back(amountYouWantToDeposit);
+    }
 }
 
-bool checkIfCorrectAccount(a, b) {
-    if (a == listOfAccountUsernames) {
+int indexOfWantedAccount(string a)
+{
+    auto indexOfTheThing = find(listOfAccountUsernames.begin(), listOfAccountUsernames.end(), a);
+    return indexOfTheThing;
+}
 
+bool checkIfCorrectAccount(string a, string b)
+{
+    bool username = false;
+    bool Password = false;
+    int lengthOfUsernameArray = sizeof(listOfAccountUsernames);
+    for (int i = 0; i < lengthOfUsernameArray; i++)
+    {
+        if (listOfAccountUsernames[i] == a)
+        {
+            username = true;
+        }
+    }
+
+    int lengthOfPasswordArray = sizeof(listOfAccountPasswords);
+    for (int j = 0; j < lengthOfPasswordArray; j++)
+    {
+        if (listOfAccountPasswords[j] == b)
+        {
+            Password = true;
+        }
+    }
+
+    if (username && Password)
+    {
+        return true;
     }
 }
